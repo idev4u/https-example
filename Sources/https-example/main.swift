152789ca116aaa96ad62d84484440dd7dc21061b
Norman Sutorius <us.idev.student@gmail.com>
2018-02-21 06:34:50 +0100

    Add Runloop

--- Sources/https-example/main.swift
+++ Sources/https-example/main.swift
@@ -1,4 +1,10 @@
 import Foundation
 
+
+let runLoop = RunLoop.current
+let distantFuture = Date.distantFuture
+
 let get = HttpsURLSession()
 get.httpGet()
+
+runLoop.run(mode: RunLoopMode.defaultRunLoopMode, before: distantFuture)
